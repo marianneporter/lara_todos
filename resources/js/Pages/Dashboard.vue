350b4cb07398d78c6d8069e7819729d48540434a
Marianne Porter <38002984+marianneporter@users.noreply.github.com>
2024-04-04 21:55:29 +0100

    wip dashboard and getters for pinia store

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,17 +3,26 @@
             <div class="max-w-6xl mx-auto mt-3">
                 <h1>Welcome to the Laravel Todo List App</h1>
              
-                <div class="flex w-full text-black gap-4 bg-fuchsia-300 min-h-full">
-                    <section class="flex-1 section-card ">
-                        <div>
-                            My Lists
-                        </div>
+                <div class="flex w-full text-black gap-4 min-h-full">
+                    <section class="flex-1 section-card ">                     
+                        <h3>My Lists</h3>
+                        <div v-for="name in getTodoListNames" :key="name">
+                            <div class="list-entry-card">
+                                <div>{{ name }}</div>
+                            </div>                         
+                        </div>                       
                     </section>
-                    <section class="flex-1 section-card">My Tasks</section>
+                    <section class="flex-1 section-card">
+                        <h3>My Todos</h3>
+                            <div v-for="todo in getTodosAllLists" :key="todo.id">
+                                <div class="list-entry-card">
+                                    <div>{{ todo.task }}</div>
+                                </div>
+                               
+                            </div>                               
+                    </section>
                 </div>
-
-            </div>
- 
+            </div> 
       </Layout>
 
 </template>
@@ -22,27 +31,20 @@
     
     import Layout from '@/Shared/Layout.vue' 
     import { useTodoListStore } from '@/Stores/todoListStore'   
-    import { onMounted } from 'vue'
-    import { usePage } from '@inertiajs/vue3'
- 
+    import { storeToRefs } from 'pinia'
   
     const props = defineProps({
         message: String,
         todoLists: Object
-    })
+    })    
 
     const todoListStore = useTodoListStore()
+
     todoListStore.setTodoLists(props.todoLists)
-    console.log(todoListStore.todoLists)
 
+    const { todoLists, getTodoListNames,
+            getTodosAllLists, getTodosForList } = storeToRefs(todoListStore)   
 
-
-    // onMounted(() => {
-    //   console.log('Component is now mounted!');
-    //   console.log(todoLists)
-    // });
-
-
-   
-
+    console.log(getTodosAllLists.value)
+ 
 </script>
--- resources/js/Stores/todoListStore.js
+++ resources/js/Stores/todoListStore.js
@@ -2,11 +2,28 @@ import { defineStore } from 'pinia'
 
 export const useTodoListStore = defineStore('taskStore', {
     state: () => ({
-        todoLists: []
-    
+        todoLists: [],
+        listSelected: 'All'
     }),
     getters: {
- 
+        getTodoListNames() {                            
+            return this.todoLists.map(tdl => tdl.name)
+        },
+
+        getTodosForList(todoListId) {   
+            let currentToDoList
+             = this.todoLists.find(t => t.id === todoListId)
+            return currentToDoList.todos 
+        },  
+
+        getTodosAllLists() {         
+            let allTodos = [];
+            this.todoLists.forEach(t => {
+                allTodos.push(...t.todos)
+            })    
+        //    console.log(allTodos)      
+            return allTodos
+        },              
     },
     actions: {
         setTodoLists(todoLists) {
