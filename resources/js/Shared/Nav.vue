6bc3111f31b2acc9897cf2f5dffeb8cd597ecb15
Marianne Porter <38002984+marianneporter@users.noreply.github.com>
2024-04-22 17:02:58 +0100

    wip responsive nav

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,25 +1,24 @@
 <template>
-    <nav class="nav bg-blue-950 text-white ">  <!-- relative container for nav -->   
+    <nav class="nav bg-blue-950 text-white lg:flex lg:justify-between">  <!-- relative container for nav -->   
           
-            <div class="title-nav w-full bg-blue-950 flex justify-between px-6 py-4">
+            <div class="title-nav w-full lg:w-auto bg-blue-950 flex justify-between px-6 py-3">
                 <h3 class="text-xl">Lara Todos</h3>
                 <button @click="menuOpen = !menuOpen"
-                        :class="{ 'open': menuOpen }" class="hamburger">
+                        :class="{ 'open': menuOpen }" class="lg:hidden hamburger">
                     <span></span>
                     <span></span>
                     <span></span>
                 </button>
-
             </div>
               
-            <div class="auth-options bg-blue-950 p-6"
+            <div class="auth-options bg-blue-950 p-6 lg:py-3"
                 :class="{'auth-options-visible': menuOpen, 
                         'auth-options-not-visible': !menuOpen }">
-                <div v-if="!username" class="flex flex-col gap-4">
+                <div v-if="!username" class="flex flex-col lg:flex-row gap-4">
                     <Link :href="route('register.show')" >Register</Link>
                     <Link :href="route('login')">Login</Link>
                 </div>   
-                <div v-else class="flex flex-col gap-4">
+                <div v-else class="flex flex-col lg:flex-row gap-4">
                     <div>Welcome {{ username }}</div>
                     <button @click="logout">
                             Log out
@@ -40,16 +39,11 @@ const username = computed(() => props.auth.username);
 
 const menuOpen = ref(false);
 
-const openHamburgerMenu = () => {
-    menuOpen.value = !menuOpen.value
-}
-
 const logout = () => {
     axios.post('/logout')
     .then(response => {
         if (response.data.success) {     
-            window.location.href = '/';
-          
+            window.location.href = '/';          
         }
     })
     .catch(error => {
@@ -59,81 +53,81 @@ const logout = () => {
 
 </script>
 
-<style scoped>
+<style scoped> 
+
+    /* mobile nav styles */
+    @media (max-width: 1023px) {
+
+        .nav {
+           position: relative; /* Ensures relative stacking context for children */
+        }
+
+        .title-nav {
+            z-index: 100; 
+            position: relative; 
+        }
+
+        .auth-options {
+            position: absolute;
+            right: 0;
+            transition: top 0.5s ease, opacity 0.1s ease 0.5s, visibility 0.1s 0.6s; 
+            z-index: 50; 
+            display: block; 
+        }
+
+        .auth-options-visible {
+            top: 100%;
+            opacity: 1;
+            transition-delay: 0s;
+            visibility: visible;
+        }
+
+        .auth-options-not-visible {
+            top: -6rem; 
+            opacity: 0;
+            visibility: hidden;
+        }
+
+        /* Style for the hamburger button */
+        .hamburger {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            width: 30px;
+            height: 25px;
+            background: transparent;
+            border: none;
+            cursor: pointer;
+            padding: 0;
+            box-sizing: border-box;
+            outline: none;
+        }
+
+        .hamburger span {
+            display: block;
+            width: 100%;
+            height: 2px;
+            background-color: white;
+            border-radius: 10px;
+            transition: all 0.3s linear;
+            position: relative;
+            transform-origin: 3px;
+        }
  
+        .hamburger.open span:first-child {
+            transform: rotate(45deg);
+            background-color: #fff; 
+        }
 
-.nav {
-    position: relative; /* Ensures relative stacking context for children */
-}
+        .hamburger.open span:nth-child(2) {
+            opacity: 0;
+            transform: translateX(20px); 
+        }
 
-.title-nav {
-    z-index: 100; /* Higher z-index to stay on top */
-    position: relative; /* This should be set to create a new stacking context for better control */
-}
-
-.auth-options {
-    position: absolute;
-    right: 0;
- /*   transition: top 0.5s ease, opacity 0.1s ease 0.5s, visibility 0.1s ease 0.6s; */ 
-    transition: top 0.5s ease, opacity 0.1s ease 0.5s, visibility 0.1s 0.6s; 
-    z-index: 50; /* Lower than title-nav but still above normal content */
-    display: block; /* Adjust visibility using opacity or visibility for smoother transitions */  
-}
-
-.auth-options-visible {
-    top: 100%; /* Adjust to drop down right below the title-nav */
-    opacity: 1;
-    transition-delay: 0s;
-    visibility: visible;
-}
-
-.auth-options-not-visible {
-    top: -6rem; /* Keep it at the same level or hide off-screen */
-    opacity: 0;
-    visibility: hidden;
-}
-
-/* Style for the hamburger button */
-.hamburger {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    width: 30px;
-    height: 25px;
-    background: transparent;
-    border: none;
-    cursor: pointer;
-    padding: 0;
-    box-sizing: border-box;
-    outline: none;
-}
-
-.hamburger span {
-    display: block;
-    width: 100%;
-    height: 2px;
-    background-color: white;
-    border-radius: 10px;
-    transition: all 0.3s linear;
-    position: relative;
-    transform-origin: 3px;
-}
-
-/* Animation for the hamburger lines into X */
-.hamburger.open span:first-child {
-    transform: rotate(45deg);
-    background-color: #fff; /* Keep the color consistent */
-}
-
-.hamburger.open span:nth-child(2) {
-    opacity: 0;
-    transform: translateX(20px); /* Push it out of visibility */
-}
-
-.hamburger.open span:last-child {
-    transform: rotate(-45deg);
-    background-color: #fff; /* Keep the color consistent */
-}
-
+        .hamburger.open span:last-child {
+            transform: rotate(-45deg);
+            background-color: #fff; 
+        }
+    }
 
 </style>
