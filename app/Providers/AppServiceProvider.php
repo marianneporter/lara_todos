4fbc1878cb9aac7f0a2e51f21ce05cf9a34a2e7b
Marianne Porter <38002984+marianneporter@users.noreply.github.com>
2024-05-17 10:40:20 +0100

    amended appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
