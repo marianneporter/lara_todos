ce209c295e6dfbdc00e827c0318bcfac4eb00f9b
Marianne Porter <38002984+marianneporter@users.noreply.github.com>
2024-05-20 16:44:46 +0100

    added vite.php file

---
+++ config/vite.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Vite Build Directory
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the directory where Vite will place the compiled
+    | assets. This will typically be in the "public/build" directory but can be
+    | modified to another location in your application's configuration.
+    |
+    */
+
+    'build_path' => env('VITE_BUILD_PATH', 'build'),
+];
